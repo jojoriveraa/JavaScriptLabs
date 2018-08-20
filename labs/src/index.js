ff5c43d526c738b9c21d02dc21a20fe87e1285fe
Jorge Rivera <Jorge.Rivera0@walmart.com>
2018-08-20 17:01:05 -0500

    part 1 challenge: eslint

--- labs/src/index.js
+++ labs/src/index.js
@@ -1,95 +1,92 @@
-alert("hello world 2");
-document.getElementById("main").innerHTML = "hello world";
+alert('hello world 2')
+document.getElementById('main').innerHTML = 'hello world'
 
-const min = 1;
-const max = 10;
-var counter = 1;
-const random = Math.floor(Math.random() * (max - min + 1)) + min;
-const random2 = Math.floor(Math.random() * (max - min + 1)) + min;
-
-console.log(random);
-console.log(random2);
+const min = 1
+const max = 10
+var counter = 1
+const random = Math.floor(Math.random() * (max - min + 1)) + min
+const random2 = Math.floor(Math.random() * (max - min + 1)) + min
 
 for (let i = 1; i <= random; i++) {
-    let num = document.createElement('p');
+    let num = document.createElement('p')
     switch (i) {
-        case 1:
-            num.textContent = "Juan";
-            break;
-        case 2:
-            num.textContent = "Fool";
-            break;
-        case 3:
-            num.textContent = "Free";
-            break;
-        case 4:
-            num.textContent = "Yorch";
-            break;
-        case 5:
-            num.textContent = "fly";
-            break;
-        case 6:
-            num.textContent = "mix";
-            break;
-        case 7:
-            num.textContent = "eleven";
-            break;
-        case 8:
-            num.textContent = "hate";
-            break;
-        case 9:
-            num.textContent = "fine";
-            break;
-        case 10:
-            num.textContent = "pen";
-            break;
-        default:
-            num.textContent = i;
-            break;
+    case 1:
+        num.textContent = 'Juan'
+        break
+    case 2:
+        num.textContent = 'Fool'
+        break
+    case 3:
+        num.textContent = 'Free'
+        break
+    case 4:
+        num.textContent = 'Yorch'
+        break
+    case 5:
+        num.textContent = 'fly'
+        break
+    case 6:
+        num.textContent = 'mix'
+        break
+    case 7:
+        num.textContent = 'eleven'
+        break
+    case 8:
+        num.textContent = 'hate'
+        break
+    case 9:
+        num.textContent = 'fine'
+        break
+    case 10:
+        num.textContent = 'pen'
+        break
+    default:
+        num.textContent = i
+        break
     }
-    document.getElementById("elements").appendChild(num);
+    document.getElementById('elements').appendChild(num)
 }
 
 
 
 while (counter <= random2) {
-    let num = document.createElement('p');
+    let num = document.createElement('p')
     switch (counter) {
-        case 1:
-            num.textContent = "Juan";
-            break;
-        case 2:
-            num.textContent = "Fool";
-            break;
-        case 3:
-            num.textContent = "Free";
-            break;
-        case 4:
-            num.textContent = "Yorch";
-            break;
-        case 5:
-            num.textContent = "fly";
-            break;
-        case 6:
-            num.textContent = "mix";
-            break;
-        case 7:
-            num.textContent = "eleven";
-            break;
-        case 8:
-            num.textContent = "hate";
-            break;
-        case 9:
-            num.textContent = "fine";
-            break;
-        case 10:
-            num.textContent = "pen";
-            break;
-        default:
-            num.textContent = counter;
-            break;
+    case 1:
+        num.textContent = 'Juan'
+        break
+    case 2:
+        num.textContent = 'Fool'
+        break
+    case 3:
+        num.textContent = 'Free'
+        break
+    case 4:
+        num.textContent = 'Yorch'
+        break
+    case 5:
+        num.textContent = 'fly'
+        break
+    case 6:
+        num.textContent = 'mix'
+        break
+    case 7:
+        num.textContent = 'eleven'
+        break
+    case 8:
+        num.textContent = 'hate'
+        break
+    case 9:
+        num.textContent = 'fine'
+        break
+    case 10:
+        num.textContent = 'pen'
+        break
+    default:
+        num.textContent = counter
+        break
     }
-    document.getElementById("elements").appendChild(num);
-    counter++;
+    document.getElementById('elements').appendChild(num)
+    counter++
 }
 
