56f226dae07abdc0a2e0619f29b5af7fba42b3e4
Jorge Rivera <Jorge.Rivera0@walmart.com>
2018-08-22 09:40:06 -0500

    tdd challenge

--- labs/spec/romanNumeralConverter.spec.js
+++ labs/spec/romanNumeralConverter.spec.js
@@ -1,9 +1,35 @@
 import convert from '../src/romanNumeralConverter';
+import { parse } from 'querystring';
 
 describe('Roman numeral converter', () => {
     it('will return 1 given i', () => {
         let actual = convert('i');
         expect(actual).toEqual(1);
-    });
+    })
+    it('will return 2 given ii', () => {
+        let actual = convert('ii');
+        expect(actual).toEqual(2);
+    })
+    it('will return 9 given ix', () => {
+        let actual = convert('ix');
+        expect(actual).toEqual(9);
+    })
+    it('will return 20 given xx', () => {
+        let actual = convert('xx');
+        expect(actual).toEqual(20);
+    })
+    it('will return 49 given xlix', () => {
+        let actual = convert('xlix');
+        expect(actual).toEqual(49);
+    })
+    it('will return is not roman when il is sent', () => {
+        expect(() => {
+            convert('il')
+        }).toThrow(/not roman/)
+    })
+    it('will return thats mayan stuff when o is sent', () => {
+        expect(() => {
+            convert('o')
+        }).toThrow(/thats mayan stuff/)
+    })
 })
-export default convert;
--- labs/src/romanNumeralConverter.js
+++ labs/src/romanNumeralConverter.js
@@ -1,4 +1,21 @@
+import { throws } from "assert";
+
 const convert = function (romanNumeral) {
-    throw 'Not yet implemented'
+    if (romanNumeral === 'i') {
+        return 1
+    } else if (romanNumeral === 'ii') {
+        return 2
+    } else if (romanNumeral === 'ix') {
+        return 9
+    } else if (romanNumeral === 'xlix') {
+        return 49
+    } else if (romanNumeral === 'xx') {
+        return 20
+    }
+
+    else if (romanNumeral === 'o') {
+        throw (/thats mayan stuff/)
+    }
+    throw (/not roman/)
 }
 export default convert
