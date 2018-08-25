a6bf1aa0a5cbc5792416a050b0eaebdd4c6176b2
Jorge Rivera <Jorge.Rivera0@walmart.com>
2018-08-25 00:31:10 -0500

    refactor

--- labs/src/index.js
+++ labs/src/index.js
@@ -1,47 +1,11 @@
 import './site.css'
 import jsonPersons from './persons.json'
 import { getPersonHtml } from './person'
-import { resolve } from 'path';
-
-const text = ['Juan', 'Fool', 'Free', 'Yorch', 'Fly', 'Mix', 'Eleven', 'Hate', 'Fine', 'Pen']
-const getRandom = ((min, max) => Math.floor(Math.random() * (max - min + 1)) + min)
-
-const getHTMLNumber = (n) => {
-    const num = document.createElement('p')
-    num.textContent = (n < 10) ? num.textContent = text[n - 1] : num.textContent = n
-    return num
-}
-
-const displayElement = (i) => {
-    document.getElementById('elements').appendChild(getHTMLNumber(i))
-}
-
-const displayWithFor = () => {
-    const min = 1
-    const max = 20
-
-    const random = getRandom(min, max)
-    for (let i = 1; i <= random; i++) {
-        displayElement(i)
-    }
-}
-
-const displayWithWhile = () => {
-    const min = 1
-    const max = 20
-
-    const random2 = getRandom(min, max)
-    let counter = 1
-    while (counter <= random2) {
-        displayElement(counter)
-        counter++
-    }
-}
 
 const getPersons = () => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            console.log("finished reading persons", jsonPersons)
+            console.log('finished reading persons', jsonPersons)
             if (jsonPersons) {
                 resolve(jsonPersons)
             } else {
@@ -52,8 +16,8 @@ const getPersons = () => {
 }
 
 const displayPeople = (persons) => {
-    const people = persons.map(person => getPersonHtml(person));
-    document.querySelector('#main').innerHTML = people.join('\n');
+    const people = persons.map(p => getPersonHtml.call(p))
+    document.querySelector('#main').innerHTML = people.join('\n')
 }
 
 const renderMain = () => {
--- labs/src/person.js
+++ labs/src/person.js
@@ -1,6 +1,6 @@
 import { readFileSync } from 'fs'
 const template = readFileSync('./src/person-card.html', 'utf8')
 
-const getPersonHtml = ((p) => eval('`' + template + '`'))
+const getPersonHtml = () => eval('`' + template + '`')
 
-export { getPersonHtml }
+export { getPersonHtml }    
