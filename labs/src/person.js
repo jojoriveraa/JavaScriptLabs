993b13b90073adc6e4c7f46eda3512e3b3cd9017
Jorge Rivera <Jorge.Rivera0@walmart.com>
2018-08-26 00:50:11 -0500

    add comments

--- labs/src/person.js
+++ labs/src/person.js
@@ -2,11 +2,11 @@ import { readFileSync } from 'fs'
 const template = readFileSync('./src/person-card.html', 'utf8')
 
 class Person {
-
+    // constructor takes a 'person' object and store with _p key
     constructor(p) {
         this._p = p
     }
-
+    // getters ans setters
     get p() { return this._p }
     get html() { return eval('`' + template + '`') }
 }
